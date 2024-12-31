12532f8b3e18bf7b192dd05508097afa58fc28f0
Mark Rudolph <mark@k8ty.app>
2024-12-31 10:10:49 -0500

    add munit in as test dependency

--- src/project.scala
+++ src/project.scala
@@ -1,3 +1,6 @@
 //> using scala "3.5.2"
-//> using jvm 23 
+//> using jvm 23
 //> using dep dev.wishingtree::branch:0.0.5
+
+//> using test.dep org.scalameta::munit:1.0.3
+// using test.dep org.testcontainers:testcontainers:1.20.4 // Add this in for testcontainers
